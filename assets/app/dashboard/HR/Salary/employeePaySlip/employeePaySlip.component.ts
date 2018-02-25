100c1a381db3ce1fa464a36d7463cfdd955f8633
Ezshipp <36017683+Ezshipp@users.noreply.github.com>
2018-02-24 22:02:13 -0500

    Add files via upload

---
+++ assets/app/dashboard/HR/Salary/employeePaySlip/employeePaySlip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employeePaySlip',
+  templateUrl: './employeePaySlip.component.html',
+  styleUrls: ['./employeePaySlip.component.css']
+})
+export class EmployeePaySlipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
